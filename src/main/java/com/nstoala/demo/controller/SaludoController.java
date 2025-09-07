e71897911cbc1940172449a4b7c4eb179f141da4
EDGAR RAMIREZ TOALA <edgartoala@gmail.com>
2025-09-07 00:45:41 -0600

    feature: se modifica build.gradle, se agrega un nuevo controller, services

---
+++ src/main/java/com/nstoala/demo/controller/SaludoController.java
@@ -0,0 +1,20 @@
+package com.nstoala.demo.controller;
+
+import com.nstoala.demo.services.SaludoServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/demo")
+public class SaludoController {
+    @Autowired
+    private SaludoServices saludoServices;
+
+    @GetMapping("/saludo/{nombre}")
+    public String saludar(@PathVariable String nombre) {
+        return saludoServices.obtenerSaludo(nombre);
+    }
+}
